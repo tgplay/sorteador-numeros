85faca4ea6471e6ad3362027fca26634d1a13be8
Tiago <tiagoalmeidasp@hotmail.com>
2025-06-16 16:57:34 -0300

    botão limpar

--- app.js
+++ app.js
@@ -1,19 +1,46 @@
 function sortear(){
-    let quantidade = parseInt(document.getElementById("quantidade").value);
-    let de = parseInt(document.getElementById("de").value);
-    let ate = parseInt(document.getElementById("ate").value);
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
 
     let sorteados = [];
     let numero;
 
     for (let i = 0; i < quantidade; i++) {
-        numero = obterNUmerosAleatorios(de, ate);
+        numero = obterNumerosAleatorios(de, ate);
+
+        while(sorteados.includes(numero)){
+            numero = obterNumerosAleatorios(de, ate);
+        }
+
         sorteados.push(numero);
     }
 
-    alert(sorteados);
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados} </label>`;
+    alterarStatusBotao();
 }
 
-function obterNUmerosAleatorios(min, max){
+function obterNumerosAleatorios(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function alterarStatusBotao() {
+    let botao = document.getElementById('btn-reiniciar');
+
+    if(botao.classList.contains('container__botao-desabilitado')){
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    } else {
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+}
+
+function reiniciar() {
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('quantidade').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+    alterarStatusBotao();
 }
